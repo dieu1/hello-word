632abc4b846fe38ceb86cf0d22c284bf94a9e33f
dieu <dieupham803@gmail.com>
2024-09-03 08:41:38 +0700

    add hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print('hello word !')
+def UCLN(a, b):
+    while b != 0:
+        temp = a
+        a = b
+        b = temp % b
+    return a
+
+def Is_comp(m, n):
+    if UCLN(m, n) == 1:
+        return ('Hai số này có nguyên tố cùng nhau.')
+    else:
+        return ('Hai số này không có nguyên tố cùng nhau.')
+
+
+m = int(input('Nhập vào số m: '))
+n = int(input('Nhập vào số n: '))
+
+print(Is_comp(m, n))
